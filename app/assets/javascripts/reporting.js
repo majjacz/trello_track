70a5ef0648fe72f239daed3fbb139d524986b02b
Karel Havlena <havlena@itlab.cz>
2013-06-08 19:40:58 +0100

    get week numbers using moments.js

--- app/assets/javascripts/reporting.js
+++ app/assets/javascripts/reporting.js
@@ -1,18 +1,3 @@
-function getWeekNumber(d) {
-    // Copy date so don't modify original
-    d = new Date(d);
-    d.setHours(0,0,0);
-    // Set to nearest Thursday: current date + 4 - current day number
-    // Make Sunday's day number 7
-    d.setDate(d.getDate() + 4 - (d.getDay()||7));
-    // Get first day of year
-    var yearStart = new Date(d.getFullYear(),0,1);
-    // Calculate full weeks to nearest Thursday
-    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7)
-    // Return array of year and week number
-    return [d.getFullYear(), weekNo];
-}
-
 $(function() {
 
   $('#calendar_button_daily').datepicker({
@@ -32,8 +17,8 @@ $(function() {
     autoclose: true,
     weekStart: 1
   }).on('changeDate', function(e){
-        var weekNumberAndYear = getWeekNumber(e.date);
-        var path = "/reporting/weekly/" + String(weekNumberAndYear[0]) + "/" + String(weekNumberAndYear[1]);
+        var date = moment(e.date)
+        var path = "/reporting/weekly/" +  String(date.day(4).year()) + "/" + String(date.isoWeek());
         Turbolinks.visit(path);
     });
 
